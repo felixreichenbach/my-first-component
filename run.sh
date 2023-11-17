766bc12dca741b7a78d41a6a5320b083b679df0b
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-11-17 15:30:07 +0100

    working version

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 # Create a virtual environment to run our code
-VENV_NAME="venv"
+VENV_NAME=".venv"
 PYTHON="$VENV_NAME/bin/python"
 ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
 
